c566f748b2e203d89108f12ce3300e1fdc9b8d54
Lee Olayvar <leeolayvar@gmail.com>
2012-05-07 23:41:06 +0000

    Changed the order of the tests to focus around app.all(). It feels more generic, and more 'right' to be tested before app.get/put/etc

--- test/routing.js
+++ test/routing.js
@@ -17,6 +17,36 @@ var before = global.before
 describe('tork', function () {
   var tork_lib = require('../lib')
   
+  describe('#all()', function () {
+    var app
+    
+    beforeEach(function () {
+      app = tork_lib()
+    })
+    
+    it('should handle a single callback', function () {
+      app.all(function () {})
+      should.not.exist(app.stack[0].method)
+      should.not.exist(app.stack[0].route)
+      app.stack[0].handler.should.be.a('function')
+    })
+    
+    it('should handle a route and a callback', function () {
+      app.all('/', function () {})
+      // Match the regex route.
+      '/'.should.match(app.stack[0].route)
+      should.not.exist(app.stack[0].method)
+      app.stack[0].handler.should.be.a('function')
+    })
+    
+    it('should properly append the stack', function () {
+      app.all('/0', function () {})
+      app.all('/1', function () {})
+      '/0'.should.match(app.stack[0].route)
+      '/1'.should.match(app.stack[1].route)
+    })
+  })
+  
   describe('#get()', function () {
     var app
     
@@ -28,40 +58,10 @@ describe('tork', function () {
       should.exist(app.get)
     })
     
-    it('should handle a single callback', function () {
-      app.get(function () {})
-      should.not.exist(app.stack[0].route)
-      app.stack[0].method.should.equal('get')
-      app.stack[0].handler.should.be.a('function')
-    })
-    
-    it('should handle a route and a callback', function () {
-      app.get('/', function () {})
-      // Match the regex route.
-      '/'.should.match(app.stack[0].route)
-      app.stack[0].method.should.equal('get')
-      app.stack[0].handler.should.be.a('function')
-    })
-    
-    it('should properly append the stack', function () {
-      app.get('/0', function () {})
-      app.get('/1', function () {})
-      '/0'.should.match(app.stack[0].route)
-      '/1'.should.match(app.stack[1].route)
-    })
-  })
-  
-  describe('#all()', function () {
-    var app
-    
-    beforeEach(function () {
-      app = tork_lib()
-    })
-    
     it('should be put in the stack', function () {
-      app.all(function () {})
+      app.get(function () {})
+      should.not.exist(app.stack[0].method)
       should.not.exist(app.stack[0].route)
-      should.not.exist(app.stack[0].get)
       app.stack[0].handler.should.be.a('function')
     })
   })
